c01bc236b279ad73a949943d05f03e0b66a86312
Yalalova Yuliana <ulianaualalova.ranepa@gmail.com>
2025-02-28 20:05:10 +0200

    add some views

--- app/src/main/java/com/example/wordtree/MainActivity.java
+++ app/src/main/java/com/example/wordtree/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.wordtree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, WordChooseActivity.class);
+            startActivity(intent);
+        });
     }
 }
