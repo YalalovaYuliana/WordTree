053bc259468bd74942ee39d6c22e880376de6253
Yalalova Yuliana <ulianaualalova.ranepa@gmail.com>
2025-03-01 16:56:02 +0200

    implement onClickListener

--- app/src/main/java/com/example/wordtree/DictionaryActivity.java
+++ app/src/main/java/com/example/wordtree/DictionaryActivity.java
@@ -1,14 +1,34 @@
 package com.example.wordtree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class DictionaryActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class DictionaryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    RecyclerView wordListView;
+    List<String> wordList = Arrays.asList("Abandon", "Ability", "Able", "Cat");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        wordListView = findViewById(R.id.wordListView);
+
+        WordAdapter adapter = new WordAdapter(wordList, this);
+        wordListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, WordCardActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/wordtree/WordAdapter.java
+++ app/src/main/java/com/example/wordtree/WordAdapter.java
@@ -1,23 +1,25 @@
 package com.example.wordtree;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder> {
 
     private List<String> wordList;
+    private View.OnClickListener listener;
 
-    public WordAdapter(List<String> wordList) {
+    public WordAdapter(List<String> wordList, View.OnClickListener listener) {
         this.wordList = wordList;
+        this.listener = listener;
     }
 
     @NonNull
@@ -32,6 +34,25 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder
     public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
         String word = wordList.get(position);
         holder.wordTextView.setText(word);
+
+        int greenColor = ContextCompat.getColor(holder.itemView.getContext(), R.color.primary_color_2);
+        int lightColor = ContextCompat.getColor(holder.itemView.getContext(), R.color.text_color_1);
+        int brownColor = ContextCompat.getColor(holder.itemView.getContext(), R.color.category);;
+
+        if (position % 2 == 0) {
+            holder.wordTextView.setBackgroundColor(greenColor);
+            holder.wordTextView.setTextColor(lightColor);
+        } else {
+            holder.wordTextView.setBackgroundColor(lightColor);
+            holder.wordTextView.setTextColor(brownColor);
+        }
+
+        holder.itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onClick(v);
+            }
+        });
+
     }
 
     @Override
